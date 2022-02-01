b7d61dbdcda0787575b39e4ac093e50dcddce2ff
MahmoodEttehad <mah.ettehad@gmail.com>
2022-01-31 19:14:09 -0700

    Add files via upload

---
+++ depth3Dhelix.py
@@ -0,0 +1,56 @@
+import numpy as np
+import graphlearning as gl
+import matplotlib.pyplot as plt
+import sklearn.datasets as datasets
+from utils import peikonal_depth
+from utils import half_helix
+import sys
+
+
+size = 10
+label_color = 'r'
+markers = ['v','s','^']
+
+frac=0.05
+seed=0
+alpha=1
+n=1000
+eps = 1
+
+
+dataset = 'helix'
+X = utils.half_helix(n)
+
+knn_ind, knn_dist = gl.weightmatrix.knnsearch(X,50)
+W = gl.weightmatrix.knn(X,10,knn_data=(knn_ind,knn_dist))
+G = gl.graph(W)
+if not G.isconnected():
+    sys.exit('Graph is not connected')
+d = np.max(knn_dist,axis=1)
+kde = (d/d.max())**(-1)
+
+alpha = 1
+k = 2
+median, depth = peikonal_depth(G, kde, frac, alpha)
+depth = depth/np.max(depth)
+depth = 1-depth
+
+
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+ax.scatter(X[:,0], X[:,1], X[:,2], s=0.1*size)
+ax.scatter(X[median,0], X[median,1], X[median,2], c=label_color, marker=markers[k], s=5*size, edgecolors='black')
+plt.axis('off')
+plt.savefig('depth_'+dataset+'_alpha%d.pdf'%alpha)
+
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+ax.scatter(X[:,0], X[:,1], X[:,2], s=size, c=depth)
+plt.axis('off')
+plt.savefig('depth_'+dataset+'.pdf')
+
+
+
+
