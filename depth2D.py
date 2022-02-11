cf718010f689ab9a48fad42d733bd7c37b99fe14
Jeff Calder <jcalder@umn.edu>
2022-02-11 15:05:06 -0600

    Updatse

--- depth2D.py
+++ depth2D.py
@@ -15,9 +15,9 @@ alpha=2
 n=1000
 eps = 1
 
-dataset = 'gaussian'
+#dataset = 'gaussian'
 #dataset = 'moon'
-#dataset = 'mixture'
+dataset = 'mixture'
 
 np.random.seed(seed)
 if dataset == 'gaussian':
@@ -54,11 +54,32 @@ for k, alpha in enumerate([-1,0,1]):
     plt.savefig('figures/depth_'+dataset+'_alpha%d.pdf'%alpha)
     plt.savefig('figures/depth_'+dataset+'_alpha%d.png'%alpha,dpi=300)
 
+    if alpha == 1:
+        dp = 0
+        idx = np.argmin(depth)
+        path = [idx]
+        while dp < 1:
+            xnId = W[idx,:].nonzero()[1]
+            wnId = depth[xnId]
+            wnMx = np.argmax(wnId)
+            idx = xnId[wnMx]
+            path += [idx]
+            dp = depth[idx]
+
+
 plt.figure()
 plt.scatter(X[:,0],X[:,1], s=size, c=depth)
-plt.scatter(X[medians[1],0],X[medians[1],1], c=label_color, marker='s', s=10*size, edgecolors='black', linewidth=2)
-plt.scatter(X[medians[0],0],X[medians[0],1], c=label_color, marker='v', s=15*size, edgecolors='black', linewidth=2)
-plt.scatter(X[medians[2],0],X[medians[2],1], c=label_color, marker='^', s=15*size, edgecolors='black', linewidth=2)
+
+for i in range(len(path)-1):
+    x1 = X[path[i],:]
+    x2 = X[path[i+1],:]
+    plt.plot([x1[0],x2[0]],[x1[1],x2[1]],c='r',zorder=1)
+ 
+plt.scatter(X[medians[1],0],X[medians[1],1], c=label_color, marker='s', s=10*size, edgecolors='black', linewidth=2,zorder=2)
+plt.scatter(X[medians[0],0],X[medians[0],1], c=label_color, marker='v', s=15*size, edgecolors='black', linewidth=2,zorder=2)
+plt.scatter(X[medians[2],0],X[medians[2],1], c=label_color, marker='^', s=15*size, edgecolors='black', linewidth=2,zorder=2)
+
+   
 plt.axis('off')
 plt.axis('square')
 plt.savefig('figures/depth_'+dataset+'.pdf')
--- depth3D.py
+++ depth3D.py
@@ -7,7 +7,7 @@ from utils import half_sphere, half_helix
 import sys
 
 
-size = 40
+size = 2
 label_color = 'r'
 markers = ['v','s','^']
 
@@ -16,9 +16,9 @@ seed=0
 alpha=1
 eps = 1
 
-#dataset = 'sphere'
+dataset = 'sphere'
 #dataset = 'helix'
-dataset = 'swissroll'
+#dataset = 'swissroll'
 
 np.random.seed(seed)
 if dataset == 'helix':
@@ -45,12 +45,39 @@ median, depth = peikonal_depth(G, kde, frac, alpha)
 depth = depth/np.max(depth)
 depth = 1-depth
 
+dp = 0
+idx = np.argmin(depth)
+path = [idx]
+while dp < 1:
+    xnId = W[idx,:].nonzero()[1]
+    wnId = depth[xnId]
+    wnMx = np.argmax(wnId)
+    idx = xnId[wnMx]
+    path += [idx]
+    dp = depth[idx]
 
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
-ax.scatter(X[:,0], X[:,1], X[:,2], s=size, c=depth)
+ax.scatter(X[:,0], X[:,1], X[:,2], s=size, c=depth,zorder=1)
 if dataset=='swissroll':
     ax.view_init(4,-77)
+
+if dataset != 'helix':
+    for i in range(len(path)-1):
+        x1 = X[path[i],:]
+        x2 = X[path[i+1],:]
+        ax.plot3D([x1[0],x2[0]],[x1[1],x2[1]],[x1[2],x2[2]],c='r',zorder=2,linewidth=2)
+     
 plt.axis('off')
+if dataset == 'sphere':
+    ax.view_init(29,-160)
+
 plt.savefig('figures/depth_'+dataset+'.pdf')
 plt.savefig('figures/depth_'+dataset+'.png',dpi=300)
+
+
+
+
+
+
+
